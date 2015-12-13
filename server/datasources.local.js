32f5dab2d68b3782c42fc13cba04fc20d44aa692
Pablo Archenti <pablo.archenti@olx.com>
2015-12-12 22:00:31 -0300

    * Change default api port to 8080

---
+++ server/datasources.local.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mysql: {
+    connector: 'mysql',
+    hostname: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'verysecret',
+    database: 'desdetul_bd',
+  }
+};
