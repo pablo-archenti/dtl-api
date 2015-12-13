1214e70b49a9421e74f9e8d1d43d12b4f3ea51f1
Pablo Archenti <pablo.archenti@olx.com>
2015-12-12 22:50:41 -0300

    * Add mysql env variables for development

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,10 +1,10 @@
 module.exports = {
   mysql: {
     connector: 'mysql',
-    hostname: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'verysecret',
-    database: 'desdetul_bd',
+    hostname: process.env.OPENSHIFT_MYSQL_DB_HOST,
+    port: process.env.OPENSHIFT_MYSQL_DB_POR,
+    user: 'adminhVSWv6V',
+    password: '_6812JSat2jc',
+    database: 'api',
   }
 };
