71f22837e6845af9df8b12d26b9eba6f4357b1b7
Pablo Archenti <pablo.archenti@olx.com>
2015-12-12 22:52:37 -0300

    * Fix mysql port for dev config

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -2,7 +2,7 @@ module.exports = {
   mysql: {
     connector: 'mysql',
     hostname: process.env.OPENSHIFT_MYSQL_DB_HOST,
-    port: process.env.OPENSHIFT_MYSQL_DB_POR,
+    port: process.env.OPENSHIFT_MYSQL_DB_PORT,
     user: 'adminhVSWv6V',
     password: '_6812JSat2jc',
     database: 'api',
