21df286093b5a0b38b90330c3c60eba6c2649e29
Pablo Archenti <pablo.archenti@olx.com>
2016-01-10 21:09:29 -0300

    * Add project/volunteer relation table

--- server/bin/autoupdate.js
+++ server/bin/autoupdate.js
@@ -3,7 +3,7 @@ var app = require(path.resolve(__dirname, '../server'));
 
 var dataSource = app.dataSources.mysql;
 
-dataSource.autoupdate(['AccessToken', 'LoginCode'], function(err) {
+dataSource.autoupdate(['AccessToken', 'LoginCode', 'VolunteerProject'], function(err) {
     if (err) throw err;
     dataSource.disconnect();
 });
