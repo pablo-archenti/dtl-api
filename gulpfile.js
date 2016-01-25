08963a93cff9db340e18b0f05b50d9cd5bcd978d
Pablo Archenti <pablo.archenti@olx.com>
2016-01-25 20:46:07 -0300

    * Rename ng client update task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task(
-    'ng-client',
+    'ng-client-update',
     shell.task(['lb-ng -m dtlClient server/server.js ../dtl-app/www/app/lib/dtl/dtl.client.js'], {verbose: true})
 );
