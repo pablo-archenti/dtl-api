5f33ca79ec3d9d16d8d687ef397ad58773a49ffd
Pablo Archenti <pablo.archenti@olx.com>
2016-01-31 18:31:35 -0300

    * Refactor project volunteer relation and permissions * Add new permissions fro volunteer project relation

--- server/bin/autoupdate.js
+++ server/bin/autoupdate.js
@@ -3,7 +3,7 @@ var app = require(path.resolve(__dirname, '../server'));
 
 var dataSource = app.dataSources.mysql;
 
-dataSource.autoupdate(['AccessToken', 'LoginCode', 'VolunteerProject'], function(err) {
+dataSource.autoupdate(['AccessToken', 'LoginCode', 'Suscription'], function(err) {
     if (err) throw err;
     dataSource.disconnect();
 });
