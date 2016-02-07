99f8b8eced27be258d143dafc27dacdc7b1ccd54
Pablo Archenti <pablo.archenti@olx.com>
2016-02-07 03:00:23 -0300

    * Add indexes definitions in LoginCode and Subscription models

--- server/bin/autoupdate.js
+++ server/bin/autoupdate.js
@@ -6,20 +6,5 @@ var models = ['AccessToken', 'LoginCode', 'Subscription'];
 
 dataSource.autoupdate(models, function(err) {
     if (err) throw err;
-
-    if (models.indexOf('Subscription') !== -1) {
-        addSubscriptionIndexes(addSubscriptionIndexesCb);
-    } else {
-        dataSource.disconnect();
-    }
-});
-
-function addSubscriptionIndexes(cb) {
-    var sql = 'ALTER TABLE Subscription ADD INDEX (volunteerId), ADD INDEX (projectId)';
-    dataSource.connector.execute(sql, [], cb);
-}
-
-function addSubscriptionIndexesCb(err, result) {
-    if (err) throw err;
     dataSource.disconnect();
-}
+});
