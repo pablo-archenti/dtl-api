3f988285502c78cba26efd1d6ea93ccdb023944c
Pablo Archenti <pablo.archenti@olx.com>
2016-02-07 02:46:33 -0300

    * Add indexes creation for Subscription model in db autoupdate

--- server/bin/autoupdate.js
+++ server/bin/autoupdate.js
@@ -2,8 +2,24 @@ var path = require('path');
 var app = require(path.resolve(__dirname, '../server'));
 
 var dataSource = app.dataSources.mysql;
+var models = ['AccessToken', 'LoginCode', 'Subscription'];
 
-dataSource.autoupdate(['AccessToken', 'LoginCode', 'Subscription'], function(err) {
+dataSource.autoupdate(models, function(err) {
+    if (err) throw err;
+
+    if (models.indexOf('Subscription') !== -1) {
+        addSubscriptionIndexes(addSubscriptionIndexesCb);
+    } else {
+        dataSource.disconnect();
+    }
+});
+
+function addSubscriptionIndexes(cb) {
+    var sql = 'ALTER TABLE Subscription ADD INDEX (volunteerId), ADD INDEX (projectId)';
+    dataSource.connector.execute(sql, [], cb);
+}
+
+function addSubscriptionIndexesCb(err, result) {
     if (err) throw err;
     dataSource.disconnect();
-});
+}
