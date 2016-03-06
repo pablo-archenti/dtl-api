a9340ad96cd42f98b5e3d7165ab58119bebea4cb
Pablo Archenti <pablo.archenti@olx.com>
2016-03-06 19:56:04 -0300

    * Add loopback-sdk-angular-cli dependency

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var shell = require('gulp-shell');
 
 gulp.task(
     'ng-client-update',
-    shell.task(['lb-ng -m dtlClient server/server.js ../dtl-app/www/app/lib/dtl/dtl.client.js'], {verbose: true})
+    shell.task(['./node_modules/loopback-sdk-angular-cli/bin/lb-ng -m dtlClient server/server.js ../dtl-app/www/app/lib/dtl/dtl.client.js'], {verbose: true})
 );
