122824dc65f59630d4141cb9bbbefcf3b0fe75d1
Pablo Archenti <pablo.archenti@olx.com>
2017-02-26 14:22:24 -0300

    New npm command for recreating testing db New path to loopback ng executable

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var shell = require('gulp-shell');
 
 gulp.task(
     'ng-client-app',
-    shell.task(['./node_modules/loopback-sdk-angular-cli/bin/lb-ng -m dtlClient server/server.js ../dtl-app/www/app/lib/dtl/dtl.client.js'], {verbose: true})
+    shell.task(['./node_modules/.bin/lb-ng -m dtlClient server/server.js ../dtl-app/www/app/lib/dtl/dtl.client.js'], {verbose: true})
 );
 
 gulp.task(
     'ng-client-admin',
-    shell.task(['./node_modules/loopback-sdk-angular-cli/bin/lb-ng -m dtlClient server/server.js client/www/app/lib/dtl.client.js'], {verbose: true})
+    shell.task(['./node_modules/.bin/lb-ng -m dtlClient server/server.js client/www/app/lib/dtl.client.js'], {verbose: true})
 );
